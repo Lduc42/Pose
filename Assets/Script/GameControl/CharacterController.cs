6300219661b43ad700f28a94218fed4d60024fe0
DucApero <duclv1@apero.vn>
2023-10-09 16:08:02 +0700

    .

---
+++ Assets/Script/GameControl/CharacterController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] CharacterStates;
+    public List<Dragger> dragger;
+
+    public int currentIndex = 0;
+    public float internalCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetDraggerComponent();
+        ActiveObject(currentIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Kiểm tra nếu người dùng nhấp chuột trái
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (Vector3.Distance(dragger[currentIndex].transform.position, dragger[currentIndex].GetMousePos()) < internalCheck)
+            {
+                Debug.Log("nhap chuot");
+            }
+            //Debug.Log("nhap chuot");
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (Vector3.Distance(dragger[currentIndex].transform.position, dragger[currentIndex].GetMousePos()) < internalCheck)
+        {
+            Debug.Log("nhap chuot");
+        }
+    }
+
+    private void GetDraggerComponent()
+    {
+        foreach(var character_states in CharacterStates)
+        {
+            dragger.Add(character_states.GetComponent<Dragger>());
+        }
+    }
+
+    protected void SwitchObject()
+    {
+        DeactiveObject(currentIndex);
+        currentIndex++;
+
+        if(currentIndex >= CharacterStates.Length)
+        {
+            currentIndex = 0;
+        }
+
+        ActiveObject(currentIndex);
+    }
+
+    protected void ActiveObject(int index)
+    {
+        CharacterStates[index].gameObject.SetActive(true);
+    }
+
+    protected void DeactiveObject(int index)
+    {
+        CharacterStates[index].gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Script/GameControl/Dragger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Dragger : MonoBehaviour {
+
+    public Vector3 _dragOffset;
+    private Camera _cam;
+
+
+    [SerializeField] private float _speed = 10;
+
+    void Awake() {
+        _cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        // Kiểm tra nếu người dùng nhấp chuột trái
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            //Debug.Log("nhap chuot");
+        }
+    }
+
+    void OnMouseDown() {
+        _dragOffset = transform.position - GetMousePos();
+        _dragOffset.z = 0;
+    }
+
+    void OnMouseDrag() {
+        transform.position = Vector3.MoveTowards(transform.position, GetMousePos() + _dragOffset, _speed * Time.deltaTime) ;
+    }
+
+    private void OnMouseUp()
+    {
+        //if (Vector3.Distance(this.transform.position, GetMousePos())< 2) Debug.Log("nhap chuot");
+    }
+
+    public Vector3 GetMousePos() {
+        var mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+}
